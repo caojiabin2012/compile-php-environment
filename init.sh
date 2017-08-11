3db2d6296fb9343dc6fc8a57280a0efc4473bcf8
caojiabin <caojiabin2012@126.com>
2017-08-11 11:40:33 +0800

    [edit]修改init.sh 默认安装全部

--- init.sh
+++ init.sh
@@ -4,9 +4,10 @@ echo -e "*****************************************" > install.log
 echo -e "*   Author:caojiabin2012@gmail.com      *" >> install.log
 echo -e "*****************************************" >> install.log
 prj_path=$(cd $(dirname $0); pwd -P)
-#bash nginx.sh
-#bash php.sh
-#bash ext/redis.sh
-#bash ext/mongodb.sh
-#bash ext/yaf.sh
+
+bash nginx.sh
+bash php.sh
+bash ext/redis.sh
+bash ext/mongodb.sh
+bash ext/yaf.sh
 bash ext/swoole.sh
