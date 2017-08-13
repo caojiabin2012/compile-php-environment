5d35922502fb9ab4b2591a3a5ad559d5c50cfca6
caojiabin2012 <caojiabin2012@126.com>
2017-08-13 15:30:53 +0800

    [add]增加nginx配置文件和安装成功后启动server

--- init.sh
+++ init.sh
@@ -6,9 +6,18 @@ echo -e "*   Author:caojiabin2012@gmail.com      *" >> install.log
 echo -e "*****************************************" >> install.log
 prj_path=$(cd $(dirname $0); pwd -P)
 
+# kill
+ps -ef | grep php-fpm| grep -v grep|awk '{print $2}' | xargs kill -9
+ps -ef | grep nginx| grep -v grep|awk '{print $2}' | xargs kill -9
+
+# install
 bash nginx.sh
 bash php.sh
 bash ext/redis.sh
 bash ext/mongodb.sh
 bash ext/yaf.sh
 bash ext/swoole.sh
+
+# start server
+$NGINX_PATH/sbin/nginx
+$PHP_PATH/sbin/php-fpm
