c9f772cbb54c0fd375a67c1e4f7a49289978234b
caojiabin <caojiabin2012@gmail.com>
2017-08-17 19:35:18 +0800

    [add]增加工具类

--- nginx.sh
+++ nginx.sh
@@ -5,29 +5,22 @@ if  [ ! -n "$prj_path" ] ;then
     prj_path=$(cd $(dirname $0); pwd -P)
 fi
 source $prj_path/config.sh
+source $prj_path/tools/bash.sh
+
 package=$prj_path/package
-echo -e nginx start `date`  >> install.log
 
 # openssl
 if which yum >/dev/null; then
-    yum install -y openssl openssl-devel
+    run_cmd "yum install -y openssl openssl-devel"
 elif which apt-get >/dev/null; then
     apt install -y libltdl-dev libssl-dev libpcre3 libpcre3-dev
 elif which brew >/dev/null; then
     echo "Darwin"
 fi
 
-if [ ! -d "$NGINX_PATH" ]; then
-    mkdir -p $NGINX_PATH
-fi
-if id -u $NGINX_USER >/dev/null 2>&1; then
-    echo "user exists"
-else
-    useradd $NGINX_USER  -s /sbin/nologin
-fi
-if [ ! -d "$package/nginx" ]; then
-    mkdir -p $package/nginx
-fi
+ensure_dir "$NGINX_PATH"
+ensure_user "$NGINX_USER"
+ensure_dir "$package/nginx"
 rm -rf $package/nginx/*
 if [ ! -f "$package/nginx-$NGINX_VERSION.tar.gz" ]; then
     wget -O $package/nginx-$NGINX_VERSION.tar.gz $NGINX_DOWNLOAD_URL 
@@ -40,8 +33,10 @@ make install
 if [ $? == 0 ]; then
     echo -e "<?php phpinfo();"  > $NGINX_PATH/html/index.php 
     cp -rf $prj_path/nginx-config/* $NGINX_PATH/
-    sed -i "s/{{NGINX_LISTEN_PORT}}/$NGINX_LISTEN_PORT/" $NGINX_PATH/conf/nginx.conf
-    sed -i "s/{{PHP_FASTCGI_LISTEN_PORT}}/$PHP_FASTCGI_LISTEN_PORT/" $NGINX_PATH/conf/nginx.conf
+    #sed -i "s/{{NGINX_LISTEN_PORT}}/$NGINX_LISTEN_PORT/" $NGINX_PATH/conf/nginx.conf
+    #sed -i "s/{{PHP_FASTCGI_LISTEN_PORT}}/$PHP_FASTCGI_LISTEN_PORT/" $NGINX_PATH/conf/nginx.conf
+    run_cmd "`sed -i "s/{{PHP_FASTCGI_LISTEN_PORT}}/$PHP_FASTCGI_LISTEN_PORT/" $NGINX_PATH/conf/nginx.conf`"
+    run_cmd "`sed -i "s/{{NGINX_LISTEN_PORT}}/$NGINX_LISTEN_PORT/" $NGINX_PATH/conf/nginx.conf`"
 else
     echo -e nginx install fail. `date` >> install.log
 fi
---
+++ tools/base.sh
@@ -0,0 +1,22 @@
+#!bin/bash
+
+function run_cmd() {
+    local t=`date`
+    echo "$t: $1"
+    eval $1
+}
+
+function ensure_dir() {
+    if [ ! -d $1 ]; then
+        run_cmd "mkdir -p $1"
+    fi
+}
+
+function ensure_user() {
+    if [ id -u $1 >/dev/null 2>&1 ]; then
+        echo "user exists"
+    else
+        run_cmd "$1 -s /sbin/nologin"
+    fi
+}
+
