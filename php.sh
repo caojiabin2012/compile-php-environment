01851c796c23031b3d1d665901591fa686fc5fff
佳宾 <caojiabin2012@gmail.com>
2017-11-07 01:46:23 -0600

    Update php.sh

--- php.sh
+++ php.sh
@@ -78,6 +78,13 @@ if [ $? == 0 ]; then
     run_cmd "ln -s $PHP_PATH/bin/* /usr/local/bin/"
     run_cmd "ln -s $PHP_PATH/sbin/* /usr/local/bin/"
     echo -e php install success. `date` >> install.log
+    # install composer
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+    php composer-setup.php
+    php -r "unlink('composer-setup.php');"
+    run_cmd "mv composer.phar /usr/local/bin/"
+    echo -e php install success. `date` >> install.log
 else
     echo -e php install fail. `date` >> install.log
 fi
