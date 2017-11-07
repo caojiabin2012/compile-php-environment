ff0b0fcb15b3e2b4bc1861195dc2c993b02a8e1e
佳宾 <caojiabin2012@gmail.com>
2017-11-07 00:37:16 -0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 #!bin/bash
 
 # php
-PHP_PATH=/usr/local/software/php/v718
-PHP_CONFIG_PATH=/usr/local/software/php/v718/etc
-PHP_DOWNLOAD_URL=http://cn.php.net/distributions/php-7.1.8.tar.bz2
-PHP_VERSION=7.1.8
+PHP_PATH=/usr/local/software/php/v7111
+PHP_CONFIG_PATH=/usr/local/software/php/v7111/etc
+PHP_DOWNLOAD_URL=http://cn.php.net/distributions/php-7.1.11.tar.bz2
+PHP_VERSION=7.1.11
 PHP_FPM_USER=fpm
 PHP_FASTCGI_LISTEN_PORT=9000
 
@@ -16,8 +16,8 @@ NGINX_LISTEN_PORT=80
 NGINX_USER=nginx
 
 # ext swoole
-PHP_EXT_SWOOLE_DOWNLOAD_URL=http://pecl.php.net/get/swoole-1.9.18.tgz
-PHP_EXT_SWOOLE_VERSION=1.9.18
+PHP_EXT_SWOOLE_DOWNLOAD_URL=http://pecl.php.net/get/swoole-1.9.22.tgz
+PHP_EXT_SWOOLE_VERSION=1.9.22
 
 # ext yaf
 PHP_EXT_YAF_DOWNLOAD_URL=http://pecl.php.net/get/yaf-3.0.5.tgz
