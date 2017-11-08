60bb497a8523ad77c98934e852dcf14292bc3b3d
caojiabin <caojiabin2012@gmail.com>
2017-11-08 13:15:36 +0800

    [edit]修改composer

--- php.sh
+++ php.sh
@@ -83,7 +83,7 @@ if [ $? == 0 ]; then
     php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
     php composer-setup.php
     php -r "unlink('composer-setup.php');"
-    run_cmd "mv composer.phar /usr/local/bin/"
+    run_cmd "mv composer.phar /usr/local/bin/composer"
     echo -e php install success. `date` >> install.log
 else
     echo -e php install fail. `date` >> install.log
