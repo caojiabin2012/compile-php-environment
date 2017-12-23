115bb61bbd7b1b0770a1f89db65aa76e185dd85f
caojiabin2012 <caojiabin2012@gmail.com>
2017-12-23 16:33:58 +0800

    [edit]merge 7.1 config.sh

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 #!bin/bash
 
 # php
-PHP_PATH=/usr/local/software/php/v5632
-PHP_CONFIG_PATH=/usr/local/software/php/v5632/etc
-PHP_DOWNLOAD_URL=http://cn.php.net/distributions/php-5.6.32.tar.bz2
-PHP_VERSION=5.6.32
+PHP_PATH=/usr/local/software/php/v7112
+PHP_CONFIG_PATH=/usr/local/software/php/v7112/etc
+PHP_DOWNLOAD_URL=http://cn.php.net/distributions/php-7.1.12.tar.bz2
+PHP_VERSION=7.1.12
 PHP_FPM_USER=fpm
 PHP_FASTCGI_LISTEN_PORT=9000
 
@@ -20,20 +20,20 @@ PHP_EXT_SWOOLE_DOWNLOAD_URL=http://pecl.php.net/get/swoole-1.9.23.tgz
 PHP_EXT_SWOOLE_VERSION=1.9.23
 
 # ext yaf
-PHP_EXT_YAF_DOWNLOAD_URL=http://pecl.php.net/get/yaf-2.3.5.tgz
-PHP_EXT_YAF_VERSION=2.3.5 
+PHP_EXT_YAF_DOWNLOAD_URL=http://pecl.php.net/get/yaf-3.0.5.tgz
+PHP_EXT_YAF_VERSION=3.0.5 
 
 # ext yac
-PHP_EXT_YAC_DOWNLOAD_URL=http://pecl.php.net/get/yac-0.9.2.tgz
-PHP_EXT_YAC_VERSION=0.9.2 
+PHP_EXT_YAC_DOWNLOAD_URL=http://pecl.php.net/get/yac-2.0.2.tgz
+PHP_EXT_YAC_VERSION=2.0.2 
 
 # ext yar
-PHP_EXT_YAR_DOWNLOAD_URL=http://pecl.php.net/get/yar-1.2.5.tgz
-PHP_EXT_YAR_VERSION=1.2.5 
+PHP_EXT_YAR_DOWNLOAD_URL=http://pecl.php.net/get/yar-2.0.3.tgz
+PHP_EXT_YAR_VERSION=2.0.3 
 
 # ext redis
-PHP_EXT_REDIS_DOWNLOAD_URL=http://pecl.php.net/get/redis-2.2.8.tgz
-PHP_EXT_REDIS_VERSION=2.2.8 
+PHP_EXT_REDIS_DOWNLOAD_URL=http://pecl.php.net/get/redis-3.1.5.tgz
+PHP_EXT_REDIS_VERSION=3.1.5 
 
 # ext mongodb 
 PHP_EXT_MONGODB_DOWNLOAD_URL=http://pecl.php.net/get/mongodb-1.3.4.tgz
