92fbeaff4e9598a59cf6758be8a49abb20dfc166
caojiabin2012 <caojiabin2012@gmail.com>
2019-04-26 18:30:05 +0800

    edit

--- config.sh
+++ config.sh
@@ -1,17 +1,17 @@
 #!bin/bash
 
 # php
-PHP_PATH=/usr/local/software/php/v7123
-PHP_CONFIG_PATH=/usr/local/software/php/v7123/etc
-PHP_DOWNLOAD_URL=http://cn.php.net/distributions/php-7.1.23.tar.bz2
-PHP_VERSION=7.1.23
+PHP_PATH=/usr/local/software/php/v7128
+PHP_CONFIG_PATH=/usr/local/software/php/v7128/etc
+PHP_DOWNLOAD_URL=http://cn.php.net/distributions/php-7.1.28.tar.bz2
+PHP_VERSION=7.1.28
 PHP_FPM_USER=fpm
 PHP_FASTCGI_LISTEN_PORT=9000
 
 # nginx
-NGINX_PATH=/usr/local/software/nginx/v1155
-NGINX_DOWNLOAD_URL=http://nginx.org/download/nginx-1.15.5.tar.gz
-NGINX_VERSION=1.15.5
+NGINX_PATH=/usr/local/software/nginx/v11511
+NGINX_DOWNLOAD_URL=http://nginx.org/download/nginx-1.15.11.tar.gz
+NGINX_VERSION=1.15.11
 NGINX_LISTEN_PORT=80
 NGINX_USER=nginx
 
@@ -22,27 +22,27 @@ PHP_EXT_SWOOLE_VERSION=1.9.23
 # ext yaf
 #PHP_EXT_YAF_DOWNLOAD_URL=http://pecl.php.net/get/yaf-3.0.7.tgz
 PHP_EXT_YAF_DOWNLOAD_URL=https://github.com/laruence/yaf/archive/yaf-3.0.7.tar.gz
-PHP_EXT_YAF_VERSION=3.0.7 
+PHP_EXT_YAF_VERSION=3.0.7
 
 # ext yac
 PHP_EXT_YAC_DOWNLOAD_URL=http://pecl.php.net/get/yac-2.0.2.tgz
-PHP_EXT_YAC_VERSION=2.0.2 
+PHP_EXT_YAC_VERSION=2.0.2
 
 # ext yar
 PHP_EXT_YAR_DOWNLOAD_URL=http://pecl.php.net/get/yar-2.0.3.tgz
-PHP_EXT_YAR_VERSION=2.0.3 
+PHP_EXT_YAR_VERSION=2.0.3
 
 # ext redis
-PHP_EXT_REDIS_DOWNLOAD_URL=http://pecl.php.net/get/redis-3.1.5.tgz
-PHP_EXT_REDIS_VERSION=4.1.1 
+PHP_EXT_REDIS_DOWNLOAD_URL=http://pecl.php.net/get/redis-4.3.0.tgz
+PHP_EXT_REDIS_VERSION=4.3.0
 
-# ext mongodb 
+# ext mongodb
 PHP_EXT_MONGODB_DOWNLOAD_URL=http://pecl.php.net/get/mongodb-1.3.4.tgz
-PHP_EXT_MONGODB_VERSION=1.3.4 
+PHP_EXT_MONGODB_VERSION=1.3.4
 
-# ext ssh2 
+# ext ssh2
 PHP_EXT_SSH2_DOWNLOAD_URL=http://pecl.php.net/get/ssh2-1.1.2.tgz
-PHP_EXT_SSH2_VERSION=1.1.2 
+PHP_EXT_SSH2_VERSION=1.1.2
 
 # lib hiredis
 LIB_HIREDIS_DOWNLOAD_URL=https://github.com/redis/hiredis/archive/v0.13.3.tar.gz
